e12d98191ab7be47725fb78b287ab49a8601edf9
Aswatth <aswatth56@gmail.com>
2023-08-17 22:35:23 +0530

    Updated contents #3: Updated project details

--- src/app/content/projects/projects.component.ts
+++ src/app/content/projects/projects.component.ts
@@ -14,22 +14,26 @@ export class ProjectsComponent implements OnInit{
       {
         name: 'Covid response app',
         link: 'https://drive.google.com/file/d/17lDcdgtHUAyH6MZ8GNqjcmv0exDYE-mn/view?usp=sharing',
-        about: ['Flutter', 'PHP', 'Google-APIs']
+        about: ['Flutter', 'PHP', 'Google-APIs'],
+        icon: 'pi pi-video',
       },
       {
         name: 'Class manager app - Angular',
         link: 'https://github.com/Aswatth/classManager-Angular',
-        about: ['Angular', 'Spring Boot', 'H2 Database', 'HTML', 'CSS', 'Typescript']
+        about: ['Angular', 'Spring Boot', 'H2 Database', 'HTML', 'CSS', 'Typescript'],
+        icon: 'pi pi-github',
       },
       {
         name: 'Class manager app - Flutter',
         link: 'https://github.com/Aswatth/classManagerApp',
-        about: ['Flutter', 'SQLite database', 'Dart']
+        about: ['Flutter', 'SQLite database', 'Dart'],
+        icon: 'pi pi-github',
       },
       {
         name: 'My Cash flow',
         link: 'https://github.com/Aswatth/myCashFlow',
-        about: ['Flutter', 'SQLite database', 'Dart']
+        about: ['Flutter', 'SQLite database', 'Dart'],
+        icon: 'pi pi-github',
       },
     ];
   }  
